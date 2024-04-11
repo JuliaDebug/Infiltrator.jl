f3b7d35a67001ffa54275ddc947e566ecc652a7d
theogf <theo.galyfajou@gmail.com>
2024-04-11 14:07:22 +0200

    Remove indentation

--- src/Infiltrator.jl
+++ src/Infiltrator.jl
@@ -112,21 +112,21 @@ Wraps expression in a try block, infiltrate if an exception is raised.
 Equivalent to:
 
 ```julia
-    try
-        expr
-    catch
-        @infiltrate
-    end
+try
+    expr
+catch
+    @infiltrate
+end
 ```
 
 If `expr` is of the form `x = f(...)`, we take `x` out of the scope 
 
 ```julia
-    x = try
-        f(...)
-    catch
-        @infiltrate
-    end  
+x = try
+    f(...)
+catch
+    @infiltrate
+end  
 ```
 
 """
